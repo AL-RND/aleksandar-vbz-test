6639254de1514d9042bc41beb8b86df308653b60
AL-RND <69843912+AL-RND@users.noreply.github.com>
2020-10-04 21:01:25 +0200

    Add files via upload

---
+++ code/Task14.sql
@@ -0,0 +1,24 @@
+SELECT DISTINCT 
+	h.GPS_Latitude as lat,
+	h.GPS_Longitude as lng,
+	h2.halt_lang as name,
+	'#ff0000' as color,
+	CONCAT('Haltepunkt ID:', fsi.halt_id_von, 'Distanz:', SQRT(POW((h.GPS_Latitude - h.GPS_Latitude ),2) + POW((h.GPS_Longitude - h.GPS_Longitude),2))) AS note,
+	SQRT(POW((h.GPS_Latitude - h.GPS_Latitude),2) + POW((h.GPS_Longitude - h.GPS_Longitude),2)) AS distanz
+	
+	
+	
+	
+FROM 	
+	vbzdat.fahrzeiten_soll_ist fsi 
+INNER JOIN 
+	vbzdat.haltepunkt h ON 
+	fsi.halt_punkt_id_von = h.halt_punkt_id 
+INNER JOIN 
+	vbzdat.haltestelle h2 ON
+	h.halt_id = h2.halt_id 
+	
+ORDER BY distanz
+
+
+LIMIT 4;
